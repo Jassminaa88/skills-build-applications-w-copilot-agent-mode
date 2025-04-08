9e481290c91086742b9d4e9552e9729f360af46d
Jassminaa88 <jstojanovic88@gmail.com>
2025-04-08 15:27:51 +0000

    Update favicon link in index.html

---
+++ octofit-tracker/frontend/src/components/Users.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+
+function Users() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('https://[REPLACE-THIS-WITH-YOUR-CODESPACE-NAME]-8000.app.github.dev/api/users')
+      .then(response => response.json())
+      .then(data => setUsers(data));
+  }, []);
+
+  return (
+    <div className="container mt-4">
+      <h1 className="text-center text-primary">Users</h1>
+      <table className="table table-striped table-bordered mt-4">
+        <thead className="table-dark">
+          <tr>
+            <th>Username</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => (
+            <tr key={user.id}>
+              <td>{user.username}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Users;
